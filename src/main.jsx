d65e211db33455c6a77de9714912ec8fae97db6e
Amrabdo74 <andkpe12@gmail.com>
2024-12-23 12:14:01 +0200

    amr

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,10 +7,9 @@ import "./i18n"; // import the i18n configuration
 import "./index.css";
 import { HelmetProvider } from "react-helmet-async";
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
- 
